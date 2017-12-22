acf6f7b15a86e568688c58d1acee1f61afe83e2b
Isabelle Villamiel <isavillamiel@utexas.edu>
2017-12-22 11:55:10 -0600

    2

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try {
+            System.out.println("Please work :)");
+        }
+        catch(Exception e){
+            System.out.println("Did it work?");
+        }
+
+    }
+}
